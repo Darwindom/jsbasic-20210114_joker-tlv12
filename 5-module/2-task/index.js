c50516b3056d4ac971b81b07f5172eb793efb21d
darwindom <joker.tlv12@gmail.com>
2021-02-05 04:35:00 +0200

    Module 5 Complete

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  btn.addEventListener('click', () => {
+    if (text.hidden == false) {
+      return text.hidden = true;
+    } else {
+      return text.hidden = false;
+    }
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
+  
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let slider = document.querySelectorAll('.carousel__slide').length;
+  let sliderWidth = carousel.offsetWidth;
+
+  let offset = 0;
+
+  arrowLeft.style.display = 'none'
+
+  arrowRight.addEventListener('click', () => {
+    offset -= sliderWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
+    arrowLeft.style.display = '';
+    if (offset == -(sliderWidth*(slider - 1))){
+      arrowRight.style.display = 'none'
+    }
+  })
+
+  arrowLeft.addEventListener('click', () => {
+
+    offset += sliderWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
+    arrowRight.style.display = '';
+    if (offset == 0){
+      arrowLeft.style.display = 'none'
+    }else{
+      offset -= sliderWidth.clientWidth;
+  }
+  })
+  
 }
