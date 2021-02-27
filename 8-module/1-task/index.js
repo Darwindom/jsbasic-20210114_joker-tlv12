82fbaf4cab1bdb4f0526615294cf8dd44815d71b
darwindom <joker.tlv12@gmail.com>
2021-02-27 13:00:37 +0200

    Final Tests

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,7 +26,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +39,39 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (document.body.offsetWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+    }
+
+    if (this.startPos === undefined) {
+      this.startPos = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+    
+    if (window.pageYOffset > this.startPos) {
+      let leftIndent = Math.min(
+        document.querySelector(".container").getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      );
+
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        top: "50px",
+        zIndex: 1e3,
+        right: "10px",
+        left: `${leftIndent}px`,
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+    }
   }
 }
